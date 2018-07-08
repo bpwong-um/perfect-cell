c899c2e4c43765c39b2e121ab91872d4b76e5412
Matt Bartos <mdbartos@umich.edu>
2018-07-07 20:34:20 -0400

    Update valve.* to move multiple (up to 2) valves and use a bitwise flag to indicate if a valve_1 and valve_2 each either have a potentiometer or reed switch

--- perfect-cell.cydsn/valve.c
+++ perfect-cell.cydsn/valve.c
@@ -7,8 +7,12 @@
  */
 
 #include "valve.h"
+#include "misc.h"
 
-uint8 potentiometer_enabled = 1u;
+#define N_VALVES      4  // 2 potentiometer; 2 reed switch
+#define N_VALVE_ITER  30 // number of iterations for dead reckoning to fully open/close
+
+uint8  potentiometer_flag = 1u;
 
 int test_valve(){
 	int p_count = 0;
@@ -35,26 +39,357 @@ int test_valve(){
     return 1;
 }
 
+int test_valves(){
+	int p_count = 0, n_valve = 0;
+	
+	for (p_count = 0; p_count < 1; p_count++){
+        
+        blink_LED(5u);
+        
+		for (n_valve = 1; n_valve <= 2; n_valve++) {
+    		
+            valve_OUT(n_valve, 1u);
+    		CyDelay(1000u);
+    		valve_OUT(n_valve, 0u);
+
+    		valve_IN(n_valve, 1u);
+    		CyDelay(1000u);
+    		valve_IN(n_valve, 0u);
+        }
+                
+	}
+    
+    //float32 tmp_pos = 0.0;
+    //move_valves(0,&tmp_pos,1,0);
+    return 1;
+}
+
+int valve_IN(int valve_flag, int value) {
+    
+    // For now, assume we only have 2 valves
+    // - if valve_flag = 1, 3, 5, 7, 9... (odd), start/stop moving Valve 1 IN
+    if (valve_flag % 2 == 1) {
+        Valve_IN_Write(value);
+    }
+    // - if valve_flag = 0, 2, 4, 6, 8... (even), start/stop moving Valve 2 IN
+    if (valve_flag % 2 == 0) {
+        Valve_2_IN_Write(value);
+    }
+    
+    return 1;
+}
+
+int valve_OUT(int valve_flag, int value) {
+    
+    // For now, assume we only have 2 valves
+    // - if valve_flag = 1, 3, 5, 7, 9... (odd), start/stop moving Valve 1 OUT
+    if (valve_flag % 2 == 1) {
+        Valve_OUT_Write(value);
+    }
+    // - if valve_flag = 0, 2, 4, 6, 8... (odd), start/stop moving Valve 2 OUT
+    if (valve_flag % 2 == 0) {
+        Valve_2_OUT_Write(value);
+    }
+    return 1;
+}
+
+int start_Valves_POS(int valve_flag, uint8 potentiometer_flag) {
+    
+    if (potentiometer_flag == 1u) {
+        /* Start the Analog MUX */
+        start_AMUX(); 
+        
+        /* Set up the multiplexer and select valve_flag input (zero-indexed) 
+           Need offset for v_bat, which is attached to the 0th input of the MUX
+        */    	
+        AMux_Select((uint8) valve_flag+1); //AMux_Select(1u);
+        
+    }   
+    // else assume we're working with a reed switch actuator (digital pulse input)
+    // else (potentiometer_flag == 0u)
+    else {
+        
+        PulseCounter_Start();
+        
+        /* Start the MUX */
+        start_Pulse_MUX(); 
+        
+        // Reset the pulse Counter
+        PulseCounter_WriteCounter(0u);
+        
+        /* Set MUX to read from input for valve_flag 
+           Need offset for autosampler, which is attached to the 0th input of the MUX
+        */
+        Pulse_MUX_Controller_Write((uint8) valve_flag);         
+    }
+    
+    /* flip on the ADC pin */
+    if (valve_flag % 2 == 1) {
+        Valve_POS_EN_Write(1u);
+    }
+    else { // assume we have 2 valves only
+        Valve_2_POS_EN_Write(1u);
+    }
+    // Delay to let the pin settle
+    CyDelay(100u);
+    
+    return 1;
+}
+
+int stop_Valves_POS(int valve_flag, uint8 potentiometer_flag) {
+    
+    /* flip off the ADC pin */
+	if (valve_flag % 2 == 1) {
+        Valve_POS_EN_Write(0u);
+    }
+    else { // assume we have 2 valves only
+        Valve_2_POS_EN_Write(0u);
+    } 
+    
+    if (potentiometer_flag == 1u) {
+        stop_AMUX();        
+    }
+    // else assume we're working with a reed switch actuator (digital pulse input)
+    // else (potentiometer_flag == 0u)
+    else {
+        stop_Pulse_MUX();         
+    }
+    return 1;
+}
+
+float32 read_Valves_POS(int valve_flag, uint8 potentiometer_flag) {
+	
+	uint16 pulses;
+    uint32 reading;
+	float32 valve_pos, v_bat;
+    
+
+    if (valve_flag) {
+        // Do nothing for now
+    }
+	
+    if (potentiometer_flag == 1u) {
+       
+    	/* Read battery voltage*/
+        v_bat = read_vbat();
+        
+        /* Read the potentiometer voltage in bits */
+        reading = VBAT_ADC_Read32();
+    	
+    	/* Calculate ratio of the potentiometer voltage to the battery voltage 
+    	   This returns the percentage that the valve is open
+    	*/
+    	valve_pos = VBAT_ADC_CountsTo_Volts(reading) / v_bat;    	   	 
+    	
+    }
+    // else assume we're working with a reed switch actuator
+    else { // (potentiometer_flag == 0u)
+        
+        /* Get the pulses and store in valve_pos */
+        pulses = PulseCounter_ReadCounter();
+        valve_pos = pulses;
+      
+    }   
+    
+	return valve_pos;
+}
+
+
+int move_valves(int valve_ref, float32* valve_pos, int valve_flag, uint8 potentiometer_flag){
+	int iter;
+	int valve_out = 0;
+	float32 d_valve;
+	
+	start_Valves_POS(valve_flag, potentiometer_flag);
+    
+    if (valve_ref == 0){
+        
+		// Push the actuator out and fully close the valve 
+        for(iter = 0; iter < N_VALVE_ITER; iter++) {
+											
+			// valve_pos maxes out at 0.88 but we'll leave
+			//   this at 0.90 to ensure the valve fully closes
+			//
+			//if(valve_pos > 0.90) {									
+			//	break;
+			//}
+								
+			valve_OUT(valve_flag, 1u);
+			CyDelay(1000u);
+			valve_OUT(valve_flag, 0u);
+            
+            *valve_pos = read_Valves_POS(valve_flag, potentiometer_flag);
+		}
+        
+        stop_Valves_POS(valve_flag, potentiometer_flag);
+		
+        // Acknowledge the command
+		valve_out = -1*iter;
+		return valve_out;
+	}
+	else if (valve_ref == 100){
+		
+        // Push the actuator out and fully close the valve 
+		for(iter = 0; iter < N_VALVE_ITER; iter++) {
+											
+			// say limit for open valve is 0.05
+			//if(valve_pos > 0.05) {									
+			//	break;
+			//}
+								
+			valve_IN(valve_flag, 1u);
+			CyDelay(1000u);
+			valve_IN(valve_flag, 0u);
+            
+            *valve_pos = read_Valves_POS(valve_flag, potentiometer_flag);
+		}        
+        
+        stop_Valves_POS(valve_flag, potentiometer_flag);
+        
+		// Acknowledge the command
+		valve_out = -1*iter;
+		return valve_out;
+	}
+	else if((valve_ref > 0 && valve_ref < 100) && potentiometer_flag){	
+														
+	    // Derivative controller
+	    // move the actuator within 5 percent of the desired pos
+		for(iter = 0; iter < 20; iter++){
+			*valve_pos = 100 * read_Valves_POS(valve_flag, potentiometer_flag);
+			d_valve = valve_ref - *valve_pos;
+								
+			// pull the actuator in if desired pos > current pos
+			// to open up the valve
+			// (e.g. desired is 95% open and valve is currently 50% open)
+			if(d_valve >= 2.5){
+				valve_IN(valve_flag, 1u);
+				CyDelay(1000u);
+				valve_IN(valve_flag, 0u);	
+			}
+		    // push the actuator out if desired pos < current pos
+			// to close the valve
+			// (e.g. desired is 15% open and valve is currently 40% open)
+			if (d_valve <= -2.5){
+				valve_OUT(valve_flag, 1u);
+				CyDelay(1000u); 
+				valve_OUT(valve_flag, 0u);
+			}
+								
+			// reached objective of getting within 1 percent
+			if (d_valve < 2.5 && d_valve > -2.5){
+				break;
+			}
+		}			
+        
+        stop_Valves_POS(valve_flag, potentiometer_flag);
+        
+		// Acknowledge the command
+		valve_out = -1*iter;
+		return valve_out;						
+	}
+    
+    stop_Valves_POS(valve_flag, potentiometer_flag);
+    
+    return valve_out;
+}
+
+
+uint8 zip_valves(char *labels[], float readings[], uint8 *array_ix, int *valve_trigger, uint8 max_size, int valve_flag){
+
+    uint8 nvars = 0, k = 0;
+    float32 *valve_pos;
+    
+    // Ensure we don't access nonexistent array index
+    // Count the number of actuators flagged
+    for (k = 0; k < N_VALVES; k++) {
+        if (0x00000001 & valve_flag >> k) {
+            
+            if (*valve_trigger >= 0) { 
+                // add one var for the acknowledgement; 
+                // another for the potentiometer/reed switch
+                nvars += 2; 
+            }
+
+            // Return if max_size is exceeded
+            if (*array_ix + nvars >= max_size) {
+                return *array_ix;
+            }            
+            
+            // For potentiometer valves
+            if (k < 2) {
+                potentiometer_flag = 1u;
+            }
+            // For reed switch actuators
+            else {
+                potentiometer_flag = 0u;
+            }
+            
+            // Move one valve at a time, all to the same desired position (valve_trigger)
+            // and then update the labels and readings
+            // - Move and Update valve 1
+            if (k % 2 == 1) {
+                if (*valve_trigger >= 0 && *valve_trigger <= 100) {
+            	    *valve_trigger = move_valves(*valve_trigger, valve_pos, 1u, potentiometer_flag);
+                    // in the future, instead of 1u, consider parsing valve_flag in move_valves()
+                    
+                    // in the future, consider char *valve_triggers[] = {"valve_trigger", "valve_2_trigger"};
+                    //                         char *valve_poses[]    = {"valve_pos", "valve_2_pos"};  
+                    labels[*array_ix] = "valve_trigger";
+                    readings[*array_ix] = -1 * k;
+                    (*array_ix) += 1;
+                }
+            
+                labels[*array_ix] = "valve_pos";
+                readings[*array_ix] = *valve_pos;
+                (*array_ix) += 1;
+            }
+            // - Move and Update valve 2
+            else { // (k % 2 == 0) 
+                if (*valve_trigger >= 0 && *valve_trigger <= 100) {
+            	    *valve_trigger = move_valves(*valve_trigger, valve_pos, 0u, potentiometer_flag);
+                    // in the future, instead of 0u, consider parsing valve_flag in move_valves()
+                    
+                    labels[*array_ix] = "valve_2_trigger";
+                    readings[*array_ix] = -1 * k;
+                    (*array_ix) += 1;
+                } 
+                
+                labels[*array_ix] = "valve_2_pos";
+                readings[*array_ix] = *valve_pos;
+                (*array_ix) += 1;                
+            }
+        }
+    }
+    
+    
+    return 0u;
+}
+
 float32 read_Valve_POS() {
 	
 	uint32 reading;
 	float32 valve_pos, v_bat;
 	
-	/* Get the battery voltage */
+    /* Start up the Analog MUX */
+    start_AMUX();
+	
+    // Get the battery voltage 
 	v_bat = read_vbat();
     
-    /* Start the Analog MUX and select second input */
+    /*
+    // Start the Analog MUX and select second input 
     AMux_Start();
     AMux_Select(1u);
 	
+	// Restore User configuration & start the ADC 
+	VBAT_ADC_Wakeup();
+	VBAT_ADC_Start(); 
+    */
+    
 	/* flip on the ADC pin */
 	Valve_POS_EN_Write(1u);
 	CyDelay(100u);
-	
-	/* Restore User configuration & start the ADC */
-	VBAT_ADC_Wakeup();
-	VBAT_ADC_Start(); 
-	
+    
 	/* Set up the multiplexer */
 	AMux_Select(1u);
 	
@@ -69,12 +404,16 @@ float32 read_Valve_POS() {
 	/* Stop the conversion & save user configuration */
 	VBAT_ADC_Sleep();
 	
-	
-	/* flip off the ADC pin */
+	/*
+	// flip off the ADC pin 
 	Valve_POS_EN_Write(0u);	
 	
-    /* Turn of the Analog MUX */
+    // Turn of the Analog MUX 
     AMux_Stop();
+    */
+    
+    // Stop the Analog MUX
+    stop_AMUX();
     
 	return valve_pos;
 }
@@ -124,7 +463,7 @@ int move_valve(int valve){
 		valve_out = -1*iter;
 		return valve_out;
 	}
-	else if((valve > 0 && valve < 100) && potentiometer_enabled){	
+	else if((valve > 0 && valve < 100) && potentiometer_flag){	
 														
 	    // Derivative controller
 	    // move the actuator within 5 percent of the desired pos
@@ -169,7 +508,7 @@ uint8 zip_valve(char *labels[], float readings[], uint8 *array_ix, int *valve_tr
     if (*valve_trigger >= 0) { 
         nvars += 1; 
     }
-    if (potentiometer_enabled == 1u) {
+    if (potentiometer_flag == 1u) {
         nvars += 1;
     }
     
@@ -190,7 +529,7 @@ uint8 zip_valve(char *labels[], float readings[], uint8 *array_ix, int *valve_tr
         (*array_ix) += 1;
     }
     
-    if (potentiometer_enabled == 1u) {
+    if (potentiometer_flag == 1u) {
 	    valve_pos = 100. * read_Valve_POS();
         
         labels[*array_ix] = "valve_pos";
